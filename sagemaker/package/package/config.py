abce51723aa13ea99646471b4d34f7c48c893fb0
Thom Lane <thom.e.lane@gmail.com>
2020-09-01 14:12:13 -0700

    Updated stack_outputs.json

--- sagemaker/package/package/config.py
+++ sagemaker/package/package/config.py
@@ -10,23 +10,23 @@ cfn_stack_outputs_filepath = Path(current_folder, '../../stack_outputs.json').re
 with open(cfn_stack_outputs_filepath) as f:
     cfn_stack_outputs = json.load(f)
 
-AWS_ACCOUNT_ID = cfn_stack_outputs['aws_account_id']
-AWS_REGION = cfn_stack_outputs['aws_region']
+AWS_ACCOUNT_ID = cfn_stack_outputs['AwsAccountId']
+AWS_REGION = cfn_stack_outputs['AwsRegion']
 
-RESOURCE_NAME = cfn_stack_outputs['explain_resource_name']
+RESOURCE_NAME = cfn_stack_outputs['ResourceName']
 
-S3_BUCKET = cfn_stack_outputs['explain_s3_bucket']
+S3_BUCKET = cfn_stack_outputs['S3Bucket']
 DATASETS_S3_PREFIX = 'datasets'
 SCHEMAS_S3_PREFIX = 'schemas'
 OUTPUTS_S3_PREFIX = 'outputs'
 EXPLANATIONS_S3_PREFIX = 'explanations'
 
-SAGEMAKER_IAM_ROLE = cfn_stack_outputs['explain_sagemaker_iam_role']
+SAGEMAKER_IAM_ROLE = cfn_stack_outputs['SagemakerIamRole']
 
-GLUE_DATABASE = cfn_stack_outputs['explain_glue_database']
-GLUE_WORKFLOW = cfn_stack_outputs['explain_glue_workflow']
+GLUE_DATABASE = cfn_stack_outputs['GlueDatabase']
+GLUE_WORKFLOW = cfn_stack_outputs['GlueWorkflow']
 
-ECR_REPOSITORY = cfn_stack_outputs['explain_ecr_repository']
+ECR_REPOSITORY = cfn_stack_outputs['EcrRepository']
 DOCKER_CONFIG = '/home/ec2-user/.docker/config.json'
 
 TAG_KEY = 'sagemaker-solution'
