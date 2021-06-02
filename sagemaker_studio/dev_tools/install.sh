dbb8ea1a685412033c774c2a79cc1e5794438cf9
Ehsan M. Kermani <kerehsan@amazon.com>
2021-06-02 15:00:40 -0700

    Remove dev_tools from studio

--- sagemaker_studio/dev_tools/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-bash ./jupytext/install.sh
-bash ./theia/install.sh
-echo "Success: restarting jupyter-server."
-sudo initctl restart jupyter-server --no-wait
--- sagemaker_studio/dev_tools/jupytext/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-source activate JupyterSystemEnv
-pip install jupytext --upgrade
-jupyter serverextension enable jupytext
-source deactivate
